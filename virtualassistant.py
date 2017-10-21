9ceed3345fc88e4cd76ce4c208136c066f344c0d
Subrata Sarkar <subrotosarkar32@gmail.com>
2017-10-21 21:21:33 +0530

    first
    update to version1.0

--- virtualassistant.py
+++ virtualassistant.py
@@ -9,6 +9,9 @@ import webbrowser
 import urllib2
 import urllib
 import re
+import psutil
+import tkMessageBox
+import speech_recognition as sr
 def load_image(file):# function to load images
         '''This function returns original image if PIL is present else runs not avaible image'''
         main_dir = os.path.split(os.path.abspath(__file__))[0]
@@ -23,11 +26,11 @@ def load_image(file):# function to load images
              return q
 
 root=Tkinter.Tk()
-root.minsize(800,450)
-
+root.minsize(840,430)
+root.title('Sara')
 #Here the main parent program strats
-
-
+img0=load_image('aiback.jpg')
+#making text box and scrollbar
 strvar=Tkinter.StringVar()
 text_frame=Tkinter.Frame(root,borderwidth=1, relief="sunken")
 text1 =Tkinter.Text(root,wrap="word",background="#2c8da9",foreground="white",undo=True,font=("siyam rupali",10),state='disabled')
@@ -41,20 +44,34 @@ text1.focus()
 text_frame.place(bordermode=OUTSIDE,height=400,width=500 )
 text1.imglist=[]
 
-text1.config(state='normal')
-text1.insert(Tkinter.INSERT,'''Welcome Sir! Sara is here at your service.\n''')
-text1.config(state='disabled')
-label=Tkinter.Label(root,relief=FLAT,background="#2c8da9",foreground="white",width=10,text='image to be supplied')
-label.place(x=510,y=0)
-button1=Tkinter.Button(root,relief=FLAT,background="#2c8da9",foreground="white",width=10,text='HELP')
-button1.place(x=510,y=380)
-button2=Tkinter.Button(root,relief=FLAT,background="#2c8da9",foreground="white",width=10,text='CREDIT')
-button2.place(x=580,y=380)
 
+label=Tkinter.Label(root,relief=FLAT,background="#2c8da9",foreground="white",text='image to be supplied',image=img0)
+label.place(x=510,y=0)
+def helpart():
+        tkMessageBox.showinfo('Help','''1.Click on  start to start button to start.\n2.Say 'search...'. to search for something.\n\
+3.Say 'play...'. to youtube for something.\n4.Say 'write....' to write to text file.\n5.Say 'find ...' to search for a file in your pc.''')
+def credit():
+        tkMessageBox.showinfo('Credit','Made by Subrata Sarkar(subrotosarkar32@gmail.com) and creditsto the makers of the packages used.')
+button2=Tkinter.Button(root,relief=FLAT,background="#2c8da9",foreground="white",width=15,text='HELP',command=helpart)
+button2.place(x=600,y=380)
+button3=Tkinter.Button(root,relief=FLAT,background="#2c8da9",foreground="white",width=23,text='CREDIT',command=credit)
+button3.place(x=710,y=380)
+def system_say(event=None,say1='Nothing passed into function'):
+        engine = pyttsx.init()
+        try:
+                engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+        except:
+                pass
+        engine.say(say1)
+        engine.runAndWait()
+        text1.config(state='normal')
+        text1.insert(Tkinter.INSERT,say1+'\n')
+        text1.config(state='disabled')
+#Main function
 def send_to_ai(event=None):
         usrsay=entrye.get()
         if usrsay=='':
-                import speech_recognition as sr
+                
 
                 # obtain audio from the microphone
                 r = sr.Recognizer()
@@ -62,7 +79,7 @@ def send_to_ai(event=None):
                     print("Say something!")
                     audio = r.listen(source)
 
-                # recognize speech using Sphinx
+                # recognize speech using Google.Note one can also try sphinx for offline sppech recognition.
                 var=''
                 try:
                         usrsay=str(r.recognize_google(audio,language='en-IN'))
@@ -80,63 +97,30 @@ def send_to_ai(event=None):
                 text1.config(state='disabled')
                 usrsaylst=usrsay.split(' ')
                 strvar.set('')
+                #work to be done when search keyword is used
                 if usrsaylst[0]=='search':
                         try:
                                print usrsaylst[2]
                                if usrsaylst[1]=='for' :
                                        link='+'.join(usrsaylst[2:])
-                                       engine = pyttsx.init()
-                                       try:
-                                               engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-                                       except:
-                                               pass
-                                       engine.say('Searching for '+' '.join(usrsaylst[2:]))
-                                       engine.runAndWait()
-                                       text1.config(state='normal')
-                                       text1.insert(Tkinter.INSERT,'Searching for '+' '.join(usrsaylst[2:])+'\n')
-                                       text1.config(state='disabled')
+                                       system_say(say1='Searching for '+' '.join(usrsaylst[2:]))
                                        webbrowser.open('https://www.google.co.in/search?q=' + link)
                                else:
                                        link='+'.join(usrsaylst[1:])
-                                       engine = pyttsx.init()
-                                       try:
-                                               engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-                                       except:
-                                               pass
-                                       engine.say('Searching for '+' '.join(usrsaylst[2:]))
-                                       engine.runAndWait()
-                                       text1.config(state='normal')
-                                       text1.insert(Tkinter.INSERT,'Searching for '+' '.join(usrsaylst[2:])+'\n')
-                                       text1.config(state='disabled')
+                                       system_say(say1='Searching for '+' '.join(usrsaylst[1:]))
                                        webbrowser.open('https://www.google.co.in/search?q=' + link)    
                         except Exception as e:
                               link='+'.join(usrsaylst[1:])
-                              engine = pyttsx.init()
-                              try:
-                                      engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-                              except:
-                                      pass
-                              engine.say('Seaching for '+' '.join(usrsaylst[1:]))
-                              engine.runAndWait()
-                              text1.config(state='normal')
-                              text1.insert(Tkinter.INSERT,'Searching for '+' '.join(usrsaylst[1:])+'\n')
-                              text1.config(state='disabled')
+                              system_say(say1='Searching for '+' '.join(usrsaylst[1:]))
                               webbrowser.open('https://www.google.co.in/search?q=' + link)
+                #work to be done when write keyword is used
                 elif usrsaylst[0]=='write':
                         file1=open(str(usrsaylst[1])+'.txt','w+')
                         file1.write(' '.join(usrsaylst[1:]))
                         file1.close()
-                        engine = pyttsx.init()
-                        try:
-                                engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-                        except:
-                                pass
-                        engine.say('''Completed writing speech to text file.''' )
-                        engine.runAndWait()
-                        text1.config(state='normal')
-                        text1.insert(Tkinter.INSERT,'''Completed writing speech to text file.\n''')
-                        text1.config(state='disabled')
+                        system_say(say1='Completed writing speech to text file.')
                         os.startfile(str(usrsaylst[1])+'.txt')
+                #work to be done when play or youtube keyword is used
                 elif usrsaylst[0]=='play' or usrsaylst[0]=='youtube':
                         link=' '.join(usrsaylst[1:])
                         url = 'https://www.youtube.com/results?'
@@ -147,38 +131,99 @@ def send_to_ai(event=None):
                         conn.close()
                         search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content)
                         print search_results
-                        engine = pyttsx.init()
-                        try:
-                                engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-                        except:
-                                pass
-                        engine.say('Searching for '+search_query)
-                        engine.runAndWait()
-                        text1.config(state='normal')
-                        text1.insert(Tkinter.INSERT,'Searching for '+' '.join(link)+'\n')
-                        text1.config(state='disabled')
+                        system_say(say1='Searching for '+search_query)
                         webbrowser.open("https://www.youtube.com/watch?v=" + search_results[0])
-                        
-                else:
-                        engine = pyttsx.init()
+                #work to be done when find keyword is used
+                elif usrsaylst[0]=='find':
                         try:
-                                engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-                        except:
-                                pass
-                        engine.say('''Sir you didn't program me for this.''' )
-                        engine.runAndWait()
+                                if 'in drive' in usrsay:
+                                      finddirfile=list(usrsay.partition(' in drive '))
+                                      finddirfile.pop(1)
+                                      findfile=' '.join(finddirfile[0].split()[1:])
+                                      finddir=finddirfile[1]
+                                      print finddirfile,findfile,finddir
+                                      dps = psutil.disk_partitions()
+                                      for dp1 in dps:
+                                          if dp1.device[0]==finddir.capitalize():
+                                              dp=dp1
+                                              break
+                                      filelst=[]
+                                      number=0
+                                      print dp.device, dp.fstype, dp.opts
+                                      for root, dirs, files in os.walk(dp.device):
+                                          for name in files:
+                                             if findfile in name:
+                                                 filepath=os.path.abspath(os.path.join(root, name))
+                                                 filelst+=[filepath]
+                                                 number+=1
+                                                 text1.config(state='normal')
+                                                 text1.insert(Tkinter.INSERT,str(number)+':'+filepath+'\n')
+                                                 text1.config(state='disabled')
+                                                 print number,filepath
+
+                                else:
+                                    file45=' '.join(usrsaylst[1:])
+                                    dps = psutil.disk_partitions()
+                                    # Only show a couple of different types of devices, for brevity
+                                    filelst=[]
+                                    number=0
+                                    dps = psutil.disk_partitions()
+                                    for dp in dps:
+                                        print(dp.device, dp.fstype, dp.opts)
+                                        for root, dirs, files in os.walk(dp.device):
+                                          for name in files:
+                                              if filefile in name:
+                                                  filepath=os.path.abspath(os.path.join(root, name))
+                                                  filelst+=[filepath]
+                                                  number+=1
+                                                  text1.config(state='normal')
+                                                  text1.insert(Tkinter.INSERT,str(number)+':'+filepath+'\n')
+                                                  text1.config(state='disabled')
+                                                  print number,filepath
+                                
+                                if len(filelst)>=1:
+                                        system_say(sayl='These are the results I found. Please say file number to open file.')
+                                        if len(filelst)!=1:
+                                                while not(usrsay.isdigit()):
+                                                        # obtain audio from the microphone
+                                                        r = sr.Recognizer()
+                                                        with sr.Microphone() as source:
+                                                            print("Say file number!")
+                                                            audio = r.listen(source)
+
+                                                        # recognize speech using Sphinx
+                                                        var=''
+                                                        try:
+                                                                usrsay=str(r.recognize_google(audio,language='en-IN'))
+                                                                print usrsay
+                                                                strvar.set(usrsay)
+                                                        except sr.UnknownValueError:
+                                                            print("Google could not understand audio")
+                                                        except sr.RequestError as e:
+                                                            print("Google error; {0}".format(e))
+                                                        os.startfile(filelst[int(usrsay)-1])
+                                        else:
+                                                os.startfile(filelst[int(usrsay)-1][0])
+                                        system_say(say1='The file has been opened.')
+                                        strvar.set('')
+                                else:
+                                        system_say(say1='I found no files containing these file name.')
+                        except Exception as e:
+                                print e
+   
+                        
+                        
+                #work to be done for unkown keyword
+                else:
+                        system_say(say1='''Sir you didn't program me for this.''')
         else:
                 pass
 
 entrye.bind("<Down>",send_to_ai)
 import pyttsx
-engine = pyttsx.init()
-try:
-     engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-except:
-        pass
-engine.say('Welcome Sir! Sara is here at your service.')
-engine.runAndWait()
+system_say(say1='Welcome Sir! Sara is here at your service.')
 print 'Start'
-send_to_ai()
+button1=Tkinter.Button(root,relief=FLAT,background="#2c8da9",foreground="white",width=15,text='SART',command=send_to_ai)
+button1.place(x=510,y=380)
+
 root.mainloop()
