c404045fa4248f03e8ced2bf0a4da8fdf6645694
Subrata Sarkar <subrotosarkar32@gmail.com>
2017-11-22 14:58:03 +0530

    some change to setup.py
    as stated

--- setup.py
+++ setup.py
@@ -18,15 +18,16 @@ try:
           packages=['sara'],
           package_data = {'sara': ['*.md', '*.jpg']},
           include_package_data = True,
-          install_requires=['PIL','psutil','speech_recognition','pyaudio','pyttsx','monotonic'],
+          install_requires=['PIL','psutil','SpeechRecognition','pyaudio','pyttsx'],
           license='GNU GPL v2 or later',
           zip_safe=True,
           classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 2.7',
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
             ]
           )
 
